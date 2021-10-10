92ebc83b88df09af9d43348bfeea1816429a4f53
Dane Sherman <danemsherman@gmail.com>
2021-10-10 12:21:34 -0500

    Command Pattern Setup with Generic List Commands

---
+++ Assets/Scripts/Command/AddToListCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+/// <summary> Generic Add to List Command for Undo/Redo </summary>
+/// <typeparam name="T">list type</typeparam>
+public class AddToListCommand<T> : ICommand
+{
+    private List<T> dataList;
+    private T data;
+
+    public AddToListCommand(List<T> dataList, T data)
+    {
+        this.dataList = dataList;
+        this.data = data;
+    }
+
+    public virtual void Execute()
+    {
+        dataList.Add(data);
+    }
+
+    public virtual void Undo()
+    {
+        dataList.RemoveAt(dataList.Count - 1);
+    }
+}
+
---
+++ Assets/Scripts/Command/CommandHistory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+public static class CommandHistory 
+{
+    private static List<ICommand> commands = new List<ICommand>();
+    private static int currentIndex = -1;
+    
+    public static void AddCommand(ICommand command)
+    {
+        //remove any commands that have been undone
+        if(currentIndex < commands.Count - 1)
+            commands.RemoveRange(currentIndex + 1, commands.Count - (currentIndex + 1));
+
+        commands.Add(command);
+        currentIndex = commands.Count - 1;
+    }
+
+    public static void Undo()
+    {
+        if (currentIndex == 0) //nothing to Undo
+            return;
+
+        commands[currentIndex].Undo();
+        currentIndex--;
+    }
+
+    public static void Redo()
+    {
+        if (currentIndex == commands.Count - 1) //nothing to Redo
+            return;
+
+        commands[currentIndex + 1].Execute();
+        currentIndex++;
+    }
+}
